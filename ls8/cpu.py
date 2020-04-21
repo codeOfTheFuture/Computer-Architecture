270bc27d2fdd87226af6a968219471eb7352d3dd
Jeff Oliver <jeffoliver3384@gmail.com>
2020-04-20 23:35:00 -0400

    instructions

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,43 @@
 
 import sys
 
+
+ADD = 0b10100000 # Add value in two registers, store result in registerA
+AND = 0b10101000 # Bitwise-AND the values registerA, registerB, store result in registerA
+CALL = 0b01010000 # Calls a subroutine a the address stored in the register
+CMP = 0b10100111 # Compare the values in two registers.
+DEC = 0b01100110 # Decrement by 1 value in a register.
+DIV = 0b10100011 # Divide the value in the first register by the val in the second, stores result in registerA
+HLT = 0b00000001 # Halt and Exit
+INC = 0b01100101 # Increment by 1 the val in a register
+INT = 0b01010010 # Issue the interrupt number stored in a register
+IRET = 0b00010011 # Return from an interrupt handler
+JEQ = 0b01010101 # If equal flag is true, jump to the address stored in a register
+JGE = 0b01011010 # If greater-than or equal flag is true, jump to the address stored in the given register
+JGT = 0b01010111 # If greater-than is true, jump to the address stored in a register
+JLE = 0b01011001 # If less-than or equal flag is true, jump to the address stored in a register
+JLT = 0b01011000 # If less-than flag is true, jump to the address stored in a register
+JMP = 0b01010100 # Jump to the address stored in a register
+JNE = 0b01010110 # If E flag is falsy, jump to the address in a register
+LD = 0b10000011 # Loads registerA with value at memory address stored in registerB
+LDI = 0b10000010 # Set the value of a register to an int.
+MOD = 0b10100100 # Divide the value in first register by value in second, remainder of result stored in registerA
+MUL = 0b10100010 # Multiply the values in two registers, store result in registerA
+NOP = 0b00000000 # Do nothing
+NOT = 0b01101001 # Perform a bitwise-NOT on value in register, storing result in register
+OR = 0b10101010 # Bitwise-OR between values in registerA and registerB, result stored in registerA
+POP = 0b01000110 # Pop the value at the top of the stack into a register
+PRA = 0b01001000 # Print alpha character val stored in a register
+PRN = 0b01000111 # Print numeric value stored in a register
+PUSH = 0b01000101 # Push val in a register on the stack
+RET = 0b00010001 # Return from subroutine
+SHL = 0b10101100 # Shift the val in registerA left by number of bits specified in registerB, filling low bits with 0
+SHR = 0b10101101 # Shift val in registerA right by the number of bits in registerB, filling high bits with 0
+ST = 0b10000100 # Store value in registerB in address stored in registerA
+SUB = 0b10100001 # Subtract the val in the second register from first, storing result in registerA
+XOR = 0b10101011 # bitwise-XOR between vals in registerA and registerB, storeing result in registerA
+
+
 class CPU:
     """Main CPU class."""
 
